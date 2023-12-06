98086e2707efb77b6178b686f8518e094befd54b
AlanaMich <143145279+AlanaMich@users.noreply.github.com>
2023-12-05 19:59:23 -0600

    segundo

---
+++ include/MarioB.hpp
@@ -0,0 +1,56 @@
+#include <SFML/Graphics.hpp>
+
+class MarioB
+{
+public:
+    MarioB(sf::Vector2f position, sf::Color color)
+    {
+        shape.setSize(sf::Vector2f(50, 50));
+        shape.setPosition(position); // Posición inicial cuadro
+        shape.setFillColor(color);
+
+        // Cargar la imagen desde un archivo
+        
+        if (!texture.loadFromFile("assets/images/dka.png"))
+        {
+        
+        }
+        this->sprite = sf::Sprite(texture);
+        this->sprite.setPosition(position); // Posición inicial sprite
+    }
+
+    void move(float offsetX, float offsetY)
+    {
+        sprite.move(offsetX, offsetY);
+        shape.move(offsetX, offsetY);
+    }
+
+    void draw(sf::RenderWindow &window)
+    {
+        // window.draw(this->shape);
+        window.draw(this->sprite);
+    }
+
+    void update(){
+        // Actualizar el frame de la animación
+        if (clock.getElapsedTime().asSeconds() >= frameTime)
+        {
+            currentFrame = (currentFrame + 1) % numFrames;
+            sprite.setTextureRect(sf::IntRect((currentFrame * 36), 0, 37, 28));
+            clock.restart();
+        }
+    }
+
+private:
+    sf::RectangleShape shape;
+    sf::Sprite sprite;
+    sf::Texture texture;
+    sf::Clock clock;
+    float frameTime = 0.1f; // Tiempo entre cada frame en segundos
+    int currentFrame = 0;
+    int numFrames = 4; // Número total de frames en la animación
+    int frameWidth = 37;
+    int frameHeight = 28;
+};
+
+double velocidad = 0.1;
--- src/main.cpp
+++ src/main.cpp
@@ -40,6 +40,117 @@ int main()
         donk.draw(window);
         window.display();
     }
+    sf::RenderWindow window(sf::VideoMode(800, 600), "DinoChrome");
+
+    MarioB mario(sf::Vector2f(400, 300), sf::Color::Red);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+            {
+                window.close();
+            }
+        }
+
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
+        {
+            mario.move(velocidad * -1, 0);
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
+        {
+            mario.move(velocidad, 0);
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
+        {
+            mario.move(0, velocidad * -1);
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
+        {
+            mario.move(0, velocidad);
+        }
+
+        // Actualizar animacion pikachu
+        mario.update();
+
+        mario.clear();
+        mario.draw(window);
+        window.display();
+    }
+
+
+    sf::RenderWindow window(sf::VideoMode(800, 600), "Reproductor de musica");
+
+    sf::Music music;
+    if (!music.openFromFile("./assets/music/musica.ogg"))
+    {
+        // Error al cargar el archivo de música
+        return -1;
+    }
+
+    // Reproducir la música
+    music.play();
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+            {
+                window.close();
+            }
+        }
+
+        window.clear();
+        // Dibujar elementos adicionales en la ventana si es necesario
+        window.display();
+
+        // Esperar hasta que la música termine
+        if (music.getStatus() != sf::Music::Playing)
+        {
+            window.close();
+        }
+    }
+        // Crear una ventana
+    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML Image");
+
+    // Cargar la imagen desde un archivo
+    sf::Texture texture;
+    if (!texture.loadFromFile("./assets/images/pikachu.png"))
+    {
+        // Manejar el error si no se puede cargar la imagen
+        return -1;
+    }
+
+    // Crear un sprite y asignarle la textura
+    sf::Sprite sprite(texture);
+
+    // Bucle principal
+    while (window.isOpen())
+    {
+        // Procesar eventos
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+            {
+                // Cerrar la ventana si se recibe el evento de cerrar
+                window.close();
+            }
+        }
+
+        // Limpiar la ventana
+        window.clear();
+
+        // Dibujar el sprite en la ventana
+        window.draw(sprite);
+
+        // Mostrar la ventana
+        window.display();
+    }
     
     return 0;
 }
